384d56126c1f087b315e4bb4974054b08f36ebb3
maxbondi777 <max.bondarev2005@gmail.com>
2025-06-29 15:57:03 +0300

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.0"
+	id("org.springframework.boot") version "3.2.7"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("application")
 }
 
 group = "com.scrappy"
